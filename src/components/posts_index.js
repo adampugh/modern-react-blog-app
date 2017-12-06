a75c0d315fcc307f42adef1ee8d4e50942766b25
Adam Pugh <adampugh@zoho.com>
2017-12-06 00:53:47 +0000

    Initial commit

---
+++ src/components/posts_index.js
@@ -0,0 +1,48 @@
+import _ from "lodash";
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchPosts } from "../actions";
+
+
+class PostsIndex extends React.Component {
+    // react always renders before api request async
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+// adds fetchPosts as an action to props to 
+// be available within component.
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
