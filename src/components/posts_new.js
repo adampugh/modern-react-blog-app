2ef195901594378b4fcbdbbe2245c253a7112c7e
Adam Pugh <adampugh@zoho.com>
2017-12-07 01:58:08 +0000

    Added Redux Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,49 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
 
 class PostsNew extends React.Component {
+    // field obj contains event handlers 
+    // field obj links the field component with the helper function
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    // contains onChange onFocus etc.
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+
+
+
     render() {
         return (
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field 
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField} 
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
