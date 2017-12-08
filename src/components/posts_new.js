aeccb05e95279799fe8401a5f296b35917f5e016
Adam Pugh <adampugh@zoho.com>
2017-12-08 00:32:56 +0000

    Added form submit functionality

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -15,23 +15,33 @@ class PostsNew extends React.Component {
                     // contains onChange onFocus etc.
                     {...field.input}
                 />
+                {field.meta.error}
             </div>
         )
     }
 
-
+    onSubmit(values) {
+        console.log(values);
+    }
 
     render() {
+        // made available via the reduxForm function
+        const { handleSubmit } = this.props;
+
+
         return (
-            <form>
+            // handleSubmit validates the form etc.
+            // after that onSubmit is called and the values
+            // are passed that we want to work with
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field 
                     label="Title"
                     name="title"
                     component={this.renderField}
                 />
                 <Field 
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -39,11 +49,36 @@ class PostsNew extends React.Component {
                     name="content"
                     component={this.renderField} 
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
 }
 
+// values is an obj that contains users input
+function validate(values) {
+    const errors = {};
+
+    //validate inputs from 'values' obj
+    if (!values.title) {
+        errors.title = "Enter a title";
+    }
+
+    if (!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+
+    if (!values.content) {
+        errors.content = "Enter some content please";
+    }
+    // if errors is empty, form will be submitted
+    return errors;
+}
+
+// whenever the user submits the form the validate 
+// function will be called
+// also adds properties to component
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
