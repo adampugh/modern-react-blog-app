027065e4533b5e7976bd41894450dbd6581effa1
Adam Pugh <adampugh@zoho.com>
2017-12-13 00:04:52 +0000

    Updated reducer with DELETE_POST case

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,13 @@
 import _ from "lodash";
-import { FETCH_POSTS, FETCH_POST } from "../actions";
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from "../actions";
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case DELETE_POST:
+            // lodash method that says if there is an object with
+            // the specified key id, then omit it
+            return _.omit(state, action.payload);
+            
         case FETCH_POST:
             // ES5 VERSION
             // const post = action.payload.data;
